0f234a503f7b6c4b0fe7dc8718cc4a08ad9121d0
Ilyas Seisov <ilyasseisov@gmail.com>
2024-03-13 11:21:58 +0500

    created Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,80 @@
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  Box,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   // hooks
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
   // local variables
   // functions
   // return
   return (
     <>
-      <h1>Navbar</h1>
+      <AppBar position='fixed'>
+        <Toolbar
+          sx={{
+            height: '80px',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          {isMobile && (
+            <IconButton
+              onClick={() => {}}
+              edge='start'
+              sx={{ color: 'inherit', outline: 'none' }}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton
+            sx={{ color: 'inherit', marginLeft: 1 }}
+            onClick={() => {}}
+          >
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <Box>
+            {!isAuthenticated ? (
+              <Button sx={{ color: 'inherit' }} onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                component={Link}
+                to='/profile/:id'
+                style={{ color: 'inherit' }}
+                onClick={() => {}}
+              >
+                {isMobile && <>My movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  src='https://pixabay.com/vectors/avatar-icon-placeholder-facebook-1577909/'
+                />
+              </Button>
+            )}
+          </Box>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
     </>
   );
 }
+// sx={{ display: { xs: 'none', sm: 'block' } }}
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
